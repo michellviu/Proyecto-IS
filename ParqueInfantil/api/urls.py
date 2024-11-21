68a0e6503dce57c607d7cd12df462b623b1ef198
AbrahamRom <127030430+AbrahamRom@users.noreply.github.com>
2024-11-21 14:42:49 -0500

    Added a serialized view

--- ParqueInfantil/api/urls.py
+++ ParqueInfantil/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import home,InstalacionView
 
 urlpatterns = [
- path('', views.home, name='home'),
+ path('', home, name='home'),
+ path('instalacion',InstalacionView.as_view())
 ]
--- ParqueInfantil/api/views.py
+++ ParqueInfantil/api/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework import generics
+from api.models import Instalacion
+from api.serializers import InstalacionSerializer
+
 
 # Create your views here.
 
+class InstalacionView(generics.ListAPIView):
+    queryset = Instalacion.objects.all()
+    serializer_class = InstalacionSerializer
+
 def home(request):
-    return HttpResponse("Hello World")
+    return HttpResponse('HOME')
