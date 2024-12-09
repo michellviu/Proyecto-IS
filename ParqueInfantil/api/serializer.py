5f45379155bc7a24ce6b00939c841f77c5c0950b
AbrahamRom <127030430+AbrahamRom@users.noreply.github.com>
2024-12-09 01:22:42 -0500

    Views to CRUD

--- ParqueInfantil/api/serializer.py
+++ ParqueInfantil/api/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models  import Instalacion, Recurso, Usuario, Padre, Educador, Administrador, Actividad_programada, Actividad
+from api.models  import Instalacion, Recurso, Usuario, Padre, Educador, Administrador, Actividad_programada, Actividad,Reservacion,Calificacion
 
 
 class InstalacionSerializer(serializers.ModelSerializer):
@@ -11,4 +11,44 @@ class InstalacionSerializer(serializers.ModelSerializer):
 class ActividadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actividad
+        fields = '__all__'
+
+class RecursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recurso
+        fields = '__all__'
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = '__all__'
+
+class PadreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Padre
+        fields = '__all__'      
+
+class EducadorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Educador
+        fields = '__all__'    
+
+class AdministradorSerializer(serializers.ModelSerializer):   
+    class Meta:
+        model = Administrador
+        fields = '__all__'
+
+class Actividad_programadaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actividad_programada
+        fields = '__all__'
+
+class ReservacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservacion
+        fields = '__all__'
+
+class CalificacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Calificacion
         fields = '__all__'
--- ParqueInfantil/api/views.py
+++ ParqueInfantil/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
-from api.models import Instalacion
-from api.serializer import InstalacionSerializer
+from api.models import Instalacion,Actividad,Recurso,Usuario,Padre,Educador,Administrador,Actividad_programada,Reservacion,Calificacion
+from api.serializer import InstalacionSerializer, ActividadSerializer, RecursoSerializer, UsuarioSerializer, PadreSerializer, EducadorSerializer, AdministradorSerializer, Actividad_programadaSerializer, ReservacionSerializer, CalificacionSerializer
 
 
 
@@ -18,3 +18,39 @@ def home(request):
 class InstalacionView(generics.ListCreateAPIView):
     serializer_class = InstalacionSerializer
     queryset = Instalacion.objects.all()
+
+class ActividadView(generics.ListCreateAPIView):
+    serializer_class = ActividadSerializer
+    queryset = Actividad.objects.all()
+
+class RecursoView(generics.ListCreateAPIView):
+    serializer_class = RecursoSerializer
+    queryset = Recurso.objects.all()
+
+class UsuarioView(generics.ListCreateAPIView):
+    serializer_class = UsuarioSerializer
+    queryset = Usuario.objects.all()
+
+class PadreView(generics.ListCreateAPIView):    
+    serializer_class = PadreSerializer
+    queryset = Padre.objects.all()
+
+class EducadorView(generics.ListCreateAPIView):
+    serializer_class = EducadorSerializer
+    queryset = Educador.objects.all()
+
+class AdministradorView(generics.ListCreateAPIView):
+    serializer_class = AdministradorSerializer
+    queryset = Administrador.objects.all()
+
+class Actividad_programadaView(generics.ListCreateAPIView):
+    serializer_class = Actividad_programadaSerializer
+    queryset = Actividad_programada.objects.all()
+
+class ReservacionView(generics.ListCreateAPIView):
+    serializer_class = ReservacionSerializer
+    queryset = Reservacion.objects.all()
+
+class CalificacionView(generics.ListCreateAPIView):
+    serializer_class = CalificacionSerializer
+    queryset = Calificacion.objects.all()
