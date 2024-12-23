7a63ebbb71675a4500c6a946c70e8177fc1b1cdb
michellviu <michellviuramirez@gmail.com>
2024-12-23 12:06:02 -0500

    Se agrego los AppServices

---
+++ ParqueInfantil/api/DomainServices/ServicesInterfaces/IInstallationService.py
@@ -0,0 +1,5 @@
+from abc import ABC, abstractmethod
+import IGenericService
+
+class IInstallationRepository(IGenericService):
+    pass
---
+++ ParqueInfantil/api/DomainServices/ServicesInterfaces/IResourceService.py
@@ -0,0 +1,5 @@
+from abc import ABC, abstractmethod
+import IGenericService
+
+class IResourceRepository(IGenericService):
+    pass
