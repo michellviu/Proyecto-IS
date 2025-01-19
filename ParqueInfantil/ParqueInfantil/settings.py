6780dc437c9fe767c020a273f1dfcfc42cc95ea8
AbrahamRom <127030430+AbrahamRom@users.noreply.github.com>
2025-01-18 23:38:23 -0500

    cors headers allow frontend

--- ParqueInfantil/ParqueInfantil/settings.py
+++ ParqueInfantil/ParqueInfantil/settings.py
@@ -134,7 +134,9 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 # REST_FRAMEWORK = {
 #     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
