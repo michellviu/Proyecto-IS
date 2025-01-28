a78aa9af043b6261a76bc9258c40b1c470bfe5bd
michellviu <michellviuramirez@gmail.com>
2025-01-28 12:22:48 -0500

    Se modifico los serializadores

--- ParqueInfantil/api/serializers/UserSerializer.py
+++ ParqueInfantil/api/serializers/UserSerializer.py
@@ -2,11 +2,8 @@ from rest_framework import serializers
 from api.models.usuario import Usuario
 
 class UserSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField()
-    username = serializers.CharField()
-    idU = serializers.IntegerField(required=False)
-    rol = serializers.CharField(required=False)
+    id = serializers.IntegerField(source='idU',required=False)
 
     class Meta:
         model = Usuario
-        fields = ['idU', 'email', 'username','rol']
+        fields = ['id', 'email', 'username','rol']
