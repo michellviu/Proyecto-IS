83ef7e2f513443a8eea7ebee54bc5cdbf4b3ad69
Eveliz08 <espinaco.eveliz@gmail.com>
2025-01-29 07:28:08 -0500

    fix: correct casing in API endpoint URLs for consistency

--- ParqueInfantil/frontend/src/pages/admin/HandlersRequests.jsx
+++ ParqueInfantil/frontend/src/pages/admin/HandlersRequests.jsx
@@ -29,7 +29,7 @@ const fetchEntities = async (setEntities) => {
 const fetchAtributes = async (entity, setAtributes) => {
     try {
         const token = `Bearer ${localStorage.getItem('AuthToken')}`;
-        const response = await fetch(`http://127.0.0.1:8000/api/atributes/${entity.toLower()}/`,{
+        const response = await fetch(`http://127.0.0.1:8000/api/atributes/${entity}/`,{
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -50,17 +50,18 @@ const fetchAtributes = async (entity, setAtributes) => {
 const fetchInstances = async (entity, setInstances, setFilteredInstances, blockNumber = 0) => {
     try {
         const token = `Bearer ${localStorage.getItem('AuthToken')}`;
-        const response = await fetch(`http://127.0.0.1:8000/api/${entity.toLower()}/?format=json`, {
+        const response = await fetch(`http://127.0.0.1:8000/api/${entity.toLowerCase()}/`, {
             method: 'GET',
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token
+            'Content-Type': 'application/json',
+            'Authorization': token
             }
         });
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
+        message.success(data[1]);
         setInstances(data);
         setFilteredInstances(data);
     } catch (error) {
@@ -73,7 +74,7 @@ const fetchInstances = async (entity, setInstances, setFilteredInstances, blockN
 const handleDeleteRequest = async (entity, instance) => {
     try {
         const token = `Bearer ${localStorage.getItem('AuthToken')}`;
-        const response = await fetch(`http://127.0.0.1:8000/api/${entity.toLower()}/${instance.Id}/`, {
+        const response = await fetch(`http://127.0.0.1:8000/api/${entity.toLowerCase()}/${instance.Id}/`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
